384e9dc8d3424c75f5d3343dbe823c9d149b24c1
한윤지 <90892851+h-yunji@users.noreply.github.com>
2024-03-20 18:47:13 +0900

    Add files via upload

---
+++ TEAM C/VGG16/윤지/2주차/test.py
@@ -0,0 +1,54 @@
+import torchvision.datasets as datasets
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
+from VGG16 import VGG16
+import torch
+import torch.nn as nn
+
+#setting
+device = torch.device("cuda:0" if torch.cuda.is_available() else 'cpu')
+print(device)
+
+batch_size = 100
+
+# Data
+transform = transforms.Compose(
+    [transforms.ToTensor(),
+     transforms.Normalize((0.5,0.5,0.5),(0.5,0.5,0.5))]
+)
+
+SVHN_test = datasets.SVHN(root="./Data/", transform=transform, target_transform=None, download=True)
+
+test_loader = DataLoader(SVHN_test, batch_size=batch_size)
+
+# Train
+model = VGG16(base_dim=64).to(device)
+model.load_state_dict(torch.load('./train_model/VGG16_100.pth'))
+
+# 맞은 개수, 전체 개수를 저장할 변수를 지정합니다.
+correct = 0
+total = 0
+
+model.eval()
+
+# 인퍼런스 모드를 위해 no_grad 해줍니다.
+with torch.no_grad():
+    # 테스트로더에서 이미지와 정답을 불러옵니다.
+    for image,label in test_loader:
+        
+        # 두 데이터 모두 장치에 올립니다.
+        x = image.to(device)
+        y= label.to(device)
+
+        # 모델에 데이터를 넣고 결과값을 얻습니다.
+        output = model.forward(x)
+        _,output_index = torch.max(output,1)
+
+        
+        # 전체 개수 += 라벨의 개수
+        total += label.size(0)
+        correct += (output_index == y).sum().float()
+    
+    # 정확도 도출
+    print("Accuracy of Test Data: {}%".format(100*correct/total))
+    # Accuracy of Test Data: 18.921058654785156%
