c1e15dc7f9c63c74ca3a4fac4dc5f98442f691e3
한윤지 <90892851+h-yunji@users.noreply.github.com>
2024-04-04 19:21:53 +0900

    Add files via upload

---
+++ TEAM C/UNet/윤지/3주차/data_read.py
@@ -0,0 +1,94 @@
+## 필요한 패키지 등록
+import os
+import numpy as np
+from PIL import Image
+import matplotlib.pyplot as plt
+
+## 데이터 불러오기
+dir_data = './3주차 UNet/datasets'
+
+name_label = 'train-labels.tif'
+name_input = 'train-volume.tif'
+
+img_label = Image.open(os.path.join(dir_data, name_label))
+img_input = Image.open(os.path.join(dir_data, name_input))
+
+ny, nx = img_label.size
+nframe = img_label.n_frames
+
+# train, validation, test set 정의. 총 30개
+nframe_train = 24
+nframe_val = 3
+nframe_test = 3
+
+# 저장될 디렉토리 설정
+dir_save_train = os.path.join(dir_data, 'train')
+dir_save_val = os.path.join(dir_data, 'val')
+dir_save_test = os.path.join(dir_data, 'test')
+
+# 디렉토리 생성
+if not os.path.exists(dir_save_train):
+    os.makedirs(dir_save_train)
+
+if not os.path.exists(dir_save_val):
+    os.makedirs(dir_save_val)
+
+if not os.path.exists(dir_save_test):
+    os.makedirs(dir_save_test)
+
+# 랜덤하게 저장하기 위해 프레임에 대하여 랜덤 인덱스 생성
+id_frame = np.arange(nframe)
+np.random.shuffle(id_frame)
+
+offset_nframe = 0
+
+# train set을 저장하는 구문
+for i in range(nframe_train):
+    img_label.seek(id_frame[i + offset_nframe])
+    img_input.seek(id_frame[i + offset_nframe])
+
+    label_ = np.asarray(img_label)
+    input_ = np.asarray(img_input)
+
+    np.save(os.path.join(dir_save_train, 'label_%03d.npy' % i), label_)
+    np.save(os.path.join(dir_save_train, 'input_%03d.npy' % i), input_)
+
+##
+offset_nframe = nframe_train
+
+# val set
+for i in range(nframe_val):
+    img_label.seek(id_frame[i + offset_nframe])
+    img_input.seek(id_frame[i + offset_nframe])
+
+    label_ = np.asarray(img_label)
+    input_ = np.asarray(img_input)
+
+    np.save(os.path.join(dir_save_val, 'label_%03d.npy' % i), label_)
+    np.save(os.path.join(dir_save_val, 'input_%03d.npy' % i), input_)
+
+## val 추가
+offset_nframe = nframe_train + nframe_val
+# test set
+for i in range(nframe_test):
+    img_label.seek(id_frame[i + offset_nframe])
+    img_input.seek(id_frame[i + offset_nframe])
+
+    label_ = np.asarray(img_label)
+    input_ = np.asarray(img_input)
+
+    np.save(os.path.join(dir_save_test, 'label_%03d.npy' % i), label_)
+    np.save(os.path.join(dir_save_test, 'input_%03d.npy' % i), input_)
+
+'''
+# dataset matplotlib으로 출력
+plt.subplot(121)
+plt.imshow(label_, cmap='gray')
+plt.title('label')
+
+plt.subplot(122)
+plt.imshow(input_, cmap='gray')
+plt.title('input')
+
+plt.show()
+'''
